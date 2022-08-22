b6cc0cebd6cad4e50e68442b57b712917a8b9066
Denys Kovika <d.v.kovika@gmail.com>
2022-08-22 11:30:33 +0300

    ver.2

--- src/main/java/com/epam/rd/autotasks/house/House.java
+++ src/main/java/com/epam/rd/autotasks/house/House.java
@@ -1,13 +1,15 @@
 package com.epam.rd.autotasks.house;
 
+import com.epam.rd.autotasks.residents.cats.Cat;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class House {
+public class House<T> {
 
-    private final List residents = new ArrayList();
+    private final List<T> residents = new ArrayList<T>();
 
-    public void enter(Object resident) {
+    public void enter(T resident) {
         residents.add(resident);
     }
 
